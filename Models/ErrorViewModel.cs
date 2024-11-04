02517714ff79c2f6017112558dbf192534cd1016
tuprok512345 <tutmgcd210555@fpt.edu.vn>
2024-11-03 22:21:23 -0500

    suaAdd project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1202.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
